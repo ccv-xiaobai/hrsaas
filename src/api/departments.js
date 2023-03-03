4b3b7f174664d3183e31dce9c3d379ab50d288be
qing <qing@qq.com>
2023-03-03 20:51:16 +0800

    添加子组件

--- src/api/departments.js
+++ src/api/departments.js
@@ -6,3 +6,25 @@ export function getDepartments() {
     url: '/company/department'
   })
 }
+
+// 删除组织架构的部门 根据id删除部门
+export function delDepartments(id) {
+  return request({
+    url: `/company/department/${id}`,
+    method: 'delete' // 满足接口restful接口规范
+    // 同样的地址 不同的方法 执行不同的业务
+    // delete 删除业务
+    // get 获取业务
+    // post 新增或者添加业务
+    // put 修改业务
+  })
+}
+
+// 新增部门
+export function addDepartments(data) {
+  return request({
+    method: 'post',
+    url: '/company/department',
+    data // axios的body参数 data
+  })
+}
--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -4,25 +4,30 @@
       <!-- 组织结构内容 -->
       <el-card class="tree-card">
         <!-- 放置结构内容 -->
-        <TreeTools :tree-node="company" :is-root="true" />
+        <tree-tools :tree-node="company" :is-root="true" @addDepts="addDepts" />
         <!--放置一个属性   这里的props和我们之前学习的父传子 的props没关系-->
         <el-tree :data="departs" :props="defaultProps" :default-expand-all="true">
           <!-- 传入内容 插槽内容 会循环多次 有多少节点 就循环多少次-->
           <!-- 作用域插槽 slot-scope='obj' 接收传递给插槽的数据 data 就是每个节点的数据对象-->
-          <TreeTools slot-scope="{data}" :tree-node="data" />
+          <tree-tools slot-scope="{data}" :tree-node="data" @delDepts="getDepartments" @addDepts="addDepts" />
         </el-tree>
       </el-card>
+      <!-- 放置新增弹层组件 -->
+      <add-dept :show-dialog="showDialog" />
     </div>
   </div>
+
 </template>
 
 <script>
 import TreeTools from './components/tree-tooles.vue'
 import { getDepartments } from '@/api/departments'
 import { tranListToTreeData } from '@/utils/index'
+import AddDept from './components/add-dept' // 引入新增部门组件
 export default {
   components: {
-    TreeTools
+    TreeTools,
+    AddDept
   },
   data() {
     return {
@@ -30,7 +35,9 @@ export default {
       departs: [],
       defaultProps: {
         label: 'name' // 表示 从这个属性显示内容
-      }
+      },
+      showDialog: false, //  默认不显示弹窗
+      node: null // 记录当前点击的node节点
     }
   },
   created() {
@@ -43,8 +50,15 @@ export default {
       this.company = { name: result.companyName, manager: '负责人' }
       this.departs = tranListToTreeData(result.depts, '') // 需要将其转化为树形结构
       console.log(result)
+    },
+    // 监听tree-tools中触发的点击添加子部门事件
+    addDepts(node) {
+      this.showDialog = true // 显示弹层
+      // 因为node是当前的点击的部门， 此时这个部门应该记录下来,
+      this.node = node
     }
   }
+
 }
 </script>
 
